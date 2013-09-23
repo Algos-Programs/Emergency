620bca3f775c4759b72cb0c02c382b8677c3c864
Marco Velluto <velluto93@gmail.com>
2013-09-23 14:33:11 +0200

    Fix bug Refresh lista notifiche. * Closed #12

--- emergency/AppDelegate.m
+++ emergency/AppDelegate.m
@@ -9,6 +9,7 @@
 #import "AppDelegate.h"
 #import <Parse/Parse.h>
 #import "Database.h"
+#import "WebViewController.h"
 
 @implementation AppDelegate
 
@@ -24,28 +25,38 @@
 							
 - (void)applicationWillResignActive:(UIApplication *)application
 {
+    //-- Sta per entrare in background.
+    NSLog(@"WillResignActive");
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
+    //-- E' entrata in background.
+    NSLog(@"DidEnterBackground");
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
+    //-- E' uscita dal backgound
+    NSLog(@"DidBecomeActive");
+    
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
+    // E' uscita da background -> ed è attiva.
+    NSLog(@"DidBecomeActive");
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
+    NSLog(@"WillTerminate");
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
@@ -82,6 +93,12 @@
     [db addNotification:str];
     NSLog(@"%@", [db allElements]);
     
+    //-- Faccio partire il programma da capo.
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"MainStoryboard" bundle:nil];
+    WebViewController *webViewController = [storyboard instantiateViewControllerWithIdentifier:@"WebViewIdentifier"];
+    webViewController = [storyboard instantiateInitialViewController];
+    self.window.rootViewController = webViewController;
+
     NSLog(@"didReceiveRemoteNotification:%@", userInfo);
 }
 
